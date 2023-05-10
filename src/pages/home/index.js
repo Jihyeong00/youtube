e364376f71eb21c34f33849832ac482c056f9881
Jihyeong00 <iolpd42@gmail.com>
2023-05-10 13:21:31 +0900

    🌃feat : intersection-observer 추가
     반응형 무한 스크롤 준비

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,23 @@
 import { usePopularVideos } from '../../hook/queries/get-video-data';
 import Thumbnail from '../../components/thumbnails/index';
 import { v4 } from 'uuid';
-
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 const MainPage = () => {
-  const { mainList, isLoading } = usePopularVideos();
-
-  return (
-    <>
-      <div
-        id="contents"
-        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  2xl:grid-cols-5 gap-2"
-      >
-        {!isLoading && console.log(mainList)}
-        {!isLoading && mainList.data.items.map((data) => <Thumbnail key={v4()} props={data} />)}
-      </div>
-    </>
-  );
+    const { ref, inView, entry } = useInView();
+    const { mainList, isLoading } = usePopularVideos();
+    return (
+        <>
+            <div
+                id="contents"
+                className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  2xl:grid-cols-5 gap-2"
+            >
+                <div ref={ref}>
+                    {!isLoading && mainList.data.items.map((data) => <Thumbnail key={v4()} props={data} />)}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default MainPage;
